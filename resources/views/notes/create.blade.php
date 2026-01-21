061b6a35204c90fd4981cbbc5df33c63524bd323
Khasseno <altay.khasenov@gmail.com>
2026-01-21 16:02:19 +0500

    add validate error message and old value saves to create.blade.php

--- resources/views/notes/create.blade.php
+++ resources/views/notes/create.blade.php
@@ -4,18 +4,27 @@
     <form action="{{ route('notes.store') }}" method="post">
         @csrf
         <div class="mt-3 mb-3">
-            <input type="text" name="title" class="form-control" id="titleInput" placeholder="Заголовок">
+            <input type="text" name="title" class="form-control" id="titleInput" value="{{ old('title') }}" placeholder="Заголовок">
+            @error('title')
+            <p class="text-danger"> {{ $message }} </p>
+            @enderror
         </div>
 
         <div class="mb-3">
-            <textarea class="form-control" name="body" id="body_input" placeholder="Какие у Вас планы?"></textarea>
+            <textarea class="form-control" name="body" id="body_input" placeholder="Какие у Вас планы?">{{ old('body') }}</textarea>
+            @error('body')
+            <p class="text-danger"> {{ $message }} </p>
+            @enderror
         </div>
 
         <div class="mb-1 form-group row">
             <label class="col-sm-2 col-form-label" for="date">Выберите дату заметки:</label>
             <div class="col-sm-10">
-                <input type="date" name="date" value="{{date('Y-m-d') }}" class="form-control" id="date">
+                <input type="date" name="date" value="{{ old('date') ? old('date') : date('Y-m-d') }}" class="form-control" id="date">
             </div>
+            @error('date')
+            <p class="text-danger"> {{ $message }} </p>
+            @enderror
         </div>
 
         <button type="submit" class="btn btn-primary">Сохранить заметку</button>
--- resources/views/notes/edit.blade.php
+++ resources/views/notes/edit.blade.php
@@ -6,11 +6,17 @@
         @method('patch')
 
         <div class="mt-3 mb-3">
-            <input type="text" name="title" value="{{ $note->title }}" class="form-control" id="titleInput" placeholder="Заголовок">
+            <input type="text" name="title" value="{{ old('title') ? old('title') : $note->title }}" class="form-control" id="titleInput" placeholder="Заголовок">
+            @error('title')
+            <p class="text-danger"> {{ $message }} </p>
+            @enderror
         </div>
 
         <div class="mb-3">
-            <textarea class="form-control" name="body" id="body_input" placeholder="Какие у Вас планы?">{{ $note->body }}</textarea>
+            <textarea class="form-control" name="body" id="body_input" placeholder="Какие у Вас планы?"> {{ old('body') ? old('body') : $note->body }}</textarea>
+            @error('body')
+            <p class="text-danger"> {{ $message }} </p>
+            @enderror
         </div>
 
         <div class="mb-1 form-group row">
@@ -19,13 +25,18 @@
                 <input
                     type="date"
                     name="date"
-                    @if($note->date)
+                    @if(old('date'))
+                        value="{{ date_format(date_create_from_format("Y-m-d", old('date')), 'Y-m-d') }}"
+                    @elseif($note->date)
                         value="{{ date_format(date_create_from_format("Y-m-d H:i:s", $note['date']), 'Y-m-d') }}"
                     @endif
                     class="form-control"
                     id="date"
                 >
             </div>
+            @error('date')
+            <p class="text-danger"> {{ $message }} </p>
+            @enderror
         </div>
 
         <button type="submit" class="btn btn-primary">Изменить заметку</button>
