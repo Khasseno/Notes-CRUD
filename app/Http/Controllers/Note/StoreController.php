6beb9a9e735944fbcfd9c5f250a73d740cd75a66
Khasseno <altay.khasenov@gmail.com>
2026-01-22 11:01:55 +0500

    Refactor: add StoreRequest and UpdateRequest

--- app/Http/Controllers/Note/StoreController.php
+++ app/Http/Controllers/Note/StoreController.php
@@ -3,19 +3,15 @@
 namespace App\Http\Controllers\Note;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Note\StoreRequest;
 use App\Models\Note;
 use Illuminate\Http\RedirectResponse;
-use Illuminate\Http\Request;
 
 class StoreController extends Controller
 {
-    public function __invoke(Request $request): RedirectResponse
+    public function __invoke(StoreRequest $request): RedirectResponse
     {
-        $note = $request->validate([
-            'title' => 'required|string|max:255',
-            'body' => 'required|string',
-            'date' => 'nullable|date|date_format:Y-m-d|after_or_equal:today',
-        ]);
+        $note = $request->validated();
 
         Note::create($note);
         return redirect()->route('notes.index');
--- app/Http/Controllers/Note/UpdateController.php
+++ app/Http/Controllers/Note/UpdateController.php
@@ -3,20 +3,15 @@
 namespace App\Http\Controllers\Note;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Note\UpdateRequest;
 use App\Models\Note;
-use Illuminate\Http\Request;
 use Illuminate\Http\RedirectResponse;
-use Illuminate\View\View;
 
 class UpdateController extends Controller
 {
-    public function __invoke(Request $request, Note $note): RedirectResponse
+    public function __invoke(UpdateRequest $request, Note $note): RedirectResponse
     {
-        $updated = $request->validate([
-            'title' => 'required|string|max:255',
-            'body' => 'required|string',
-            'date' => 'nullable|date|date_format:Y-m-d|after_or_equal:today'
-        ]);
+        $updated = $request->validated();
 
         $note->updateOrFail($updated);
 
