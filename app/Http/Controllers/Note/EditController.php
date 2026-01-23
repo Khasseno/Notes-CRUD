cc44f3f14553673e8970a48859a7a54ef2e26fbc
Khasseno <altay.khasenov@gmail.com>
2026-01-23 15:25:09 +0500

    add user control to EditController

--- app/Http/Controllers/Note/EditController.php
+++ app/Http/Controllers/Note/EditController.php
@@ -2,14 +2,20 @@
 
 namespace App\Http\Controllers\Note;
 
-use App\Http\Controllers\Controller;
 use App\Models\Note;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\View\View;
 
 class EditController extends BaseController
 {
-    public function __invoke(Note $note): View
+    public function __invoke(Note $note): View | RedirectResponse
     {
+        $user = auth()->user();
+        if ($note->user->id !== $user->id)
+        {
+            return redirect()->route('notes.index');
+        }
+
         return view('notes.edit', compact('note'));
     }
 
