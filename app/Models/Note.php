d3acba488ec2145b18c1eeb9c17e193df86e6cfc
Khasseno <altay.khasenov@gmail.com>
2026-02-03 15:56:32 +0500

    add columns names to models

--- app/Models/Note.php
+++ app/Models/Note.php
@@ -12,7 +12,15 @@ class Note extends Model
     use HasFactory;
     use Filterable;
     use SoftDeletes;
-    protected $guarded = [];
+    protected $fillable = [
+        'id',
+        'title',
+        'body',
+        'user_id',
+        'date',
+        'created_at',
+        'updated_at'
+    ];
 
     public function user() {
         return $this->belongsTo(User::class);
